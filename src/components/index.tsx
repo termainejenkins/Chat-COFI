0f02a343d0d32820e0451d15910280521860eb69
TJ <termainejenkins@gmail.com>
2023-12-01 18:04:16 -0500

    removed comments

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+// components/index.tsx
+export { default as App } from './App';
+export { default as InputForm } from './InputForm';
+export { default as Result } from './Result';
+export { pangramCheck } from './PangramUtils';
